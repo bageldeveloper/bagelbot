460d453067ec1e25f064b30e297820e5483a1305
bagel-man <tom@blowmage.com>
2022-08-03 15:15:47 -0600

    IT WORKED LESS GOGOGOGOGO

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = commands.Bot(command_prefix='()', help_command=None)
 
 @bot.command()
 async def help(ctx):
-    await ctx.reply(f'```\n()help: types a list of commands, the thing you just did``````\n()bagel: find it out for yourself``````\n()spam: enter in the message you want to send and the number of times to send it and it will spam you with that``````\n(also {ctx.author} is pretty cringe ngl)```')
+    await ctx.reply(f'```\n()help: types a list of commands, the thing you just did``````\n()bagel: find it out for yourself``````\n()spam: enter in the message you want to send and the number of times to send it and it will spam you with that``````\nalso {ctx.author} is pretty cringe ngl```')
 
 @bot.command()
 async def bagel(ctx):
